10a2d62753c46d674eb920d79c122e5be9c77d15
Muhammad Hamza Sabir <hamza.sabir@royalcyber.com>
2024-07-11 20:34:37 +0500

    Added logging & exception script

---
+++ src/exception.py
@@ -0,0 +1,24 @@
+import sys
+import logging
+
+
+def get_error_details(error, error_detail:sys):
+
+    _,_,exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_no = exc_tb.tb_lineno
+
+    error_details = f"Error Occured in Python Script: {file_name}, Line No.: {line_no} & Error: {str(error)}"
+
+    return error_details
+
+
+class CustomException(Exception):
+    def __init__(self, error, error_detail:sys):
+        super().__init__(error)
+        self.error = get_error_details(error, error_detail) 
+
+    def __str__(self):
+        return self.error
+
+        
